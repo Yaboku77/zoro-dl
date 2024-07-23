713069f8231adc5c46a834b29d5bcb9a7eea32d9
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2024-07-23 12:40:42 +0530

    Update processor.py

--- zoro_dl/processor.py
+++ zoro_dl/processor.py
@@ -121,9 +121,9 @@ class ZORO:
             else "{}".format(self.season)
         )
         title_episode = (
-            "0{}".format(self.complete_data["episode"])
-            if int(self.complete_data["episode"]) < 10
-            else "{}".format(self.complete_data["episode"])
+            "0{}".format(episode_number)
+            if int(episode_number) < 10
+            else "{}".format(episode_number)
         )
 
         watch_id = episode["id"].split("$episode")[0]
@@ -390,9 +390,15 @@ class ZORO:
 
         _, height = get_video_resolution(out_name)
 
-        final_out_name = os.path.join(save_dir, f"{title_episode}. {self.complete_data['episodeTitle']}.mkv")
+        final_out_name = os.path.join(save_dir, "{gr} {name} [{resolution}p] [{audio}] [{subs}].mkv".format(
+            gr=self.custom_group_tag,
+            name=self.complete_data["name"],
+            resolution=height,
+            audio=self.lang_file_name_data,
+            subs=self.subs_file_name_data if self.subs_file_name_data != "NO-SUBS" else "",
+        ))
 
-        os.rename(out_name, final_out_name)  # Rename to final file name
+        os.rename(out_name, final_out_name)  # Rename to final file name within the directory
 
         return final_out_name
 
